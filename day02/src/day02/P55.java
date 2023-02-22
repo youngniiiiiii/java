312a4231b654c41639c4c480849af930cc0db0b3
youngniiiiiii <youngnikk4929@gmail.com>
2023-02-22 18:47:34 +0900

    2023-02-23 homework

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		int a = 2000000000;
+		int b = 2000000000;
+		long c = a;
+		long d = b;
+		System.out.print(c+d);
+	}
+
+}
