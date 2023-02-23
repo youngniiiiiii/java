2f139eb7ce39bf1ee74926088dd0dff9616a50e6
youngniiiiiii <124110329+youngniiiiiii@users.noreply.github.com>
2023-02-23 17:04:43 +0900

    Update P68.java
    code change

--- day02/src/variable/P68.java
+++ day02/src/variable/P68.java
@@ -11,12 +11,9 @@ public class P68 {
 		int a = 10;
 		System.out.println(s1 + a); // 1010이 나옴....
 		System.out.println(s2 + a); // 20이 나옴..
-
 		int n1 = 10; // int를 String으로 바꾸려면..
-		// String n2 = n1 + "";
 		String n2 = String.valueOf(n1);
-		System.out.println(n2);
-
+		System.out.println(n2);// String n2 = n1 + "";
 		Random r = new Random();  //48비트의 시드를 이용해서 랜덤으로 숫자를 발생시킴
 		int rndNum = r.nextInt(10)+1 ;  //10을 제외한 0~9까지 , +1 하면 1~10까지
 		System.out.println(rndNum);
