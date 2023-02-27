ab6ae8ee37358dbfe65a73009d14aaf872a1c3ba
youngniiiiiii <youngnikk4929@gmail.com>
2023-02-27 10:13:34 +0900

    del

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -3,19 +3,20 @@ package whilecontrol;
 public class P99 {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int bb =1090;
-		int ab =100;
-		
-		int d =1 ;
-		int c =1000;
+			
+			myfor:
 			for(int i=2;i<10;i++) {
 				System.out.printf("%d 단 \n",i);
 				
 				for(int j=1;j<10;j++) {
-					System.out.printf("%d * %d = %d \n",i,j,(i*j));
+					System.out.printf("%d * %d = %d \t",i,j,(i*j));
+					if((i*j)==21) {
+						break myfor;
+					}
 				}
+				System.out.println("");
 			}
+			
 
 	}
 
